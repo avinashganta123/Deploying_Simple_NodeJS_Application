2865655dc4be5d15bcc7c3c796999f0335aff873
avinash <avinashganta123@gmail.com>
2024-12-05 11:21:36 +0530

    commit

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-pm2 stop all || true
+# pm2 stop all || true
 cd /opt/deployin-node-application/my-node-app
-npm start
+# npm start
+
+#install node modules
+npm install
+npm audit fix
+#start our node app in the background
+#node app.js > app.out.log 2> app.err.log < /dev/null & 
+npm start &>> output.log & 
