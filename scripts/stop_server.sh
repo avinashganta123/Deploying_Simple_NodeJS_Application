c17ce24da5840bcac7eafcfcf92c99f1237f3897
avinash <avinashganta123@gmail.com>
2024-12-05 15:03:36 +0530

    commit

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -4,8 +4,8 @@
 
 
 #!/bin/bash
-set -e
-echo "Stopping the Node.js application..."
-# pkill -f "node /opt/codedeploy/my-nodejs-app/index.js" || true
+# set -e
+# echo "Stopping the Node.js application..."
+# # pkill -f "node /opt/codedeploy/my-nodejs-app/index.js" || true
 
-pkill -f "node/opt/deployin-node-application/my-node-app/index.js" || true
+# pkill -f "node/opt/deployin-node-application/my-node-app/index.js" || true
